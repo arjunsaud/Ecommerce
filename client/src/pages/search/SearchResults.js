f945020ca5fead950329f9adc2308fb828e01a7d
arjunsaud <arjundk01@gmail.com>
2023-01-10 12:51:31 +0545

    searchdone

--- client/src/pages/search/SearchResults.js
+++ client/src/pages/search/SearchResults.js
@@ -2,20 +2,36 @@ import axios from "../../config/axios";
 import React, { useContext, useEffect, useState } from "react";
 import "../../assets/search.css";
 import { GlobalContext } from "../../context/GlobalContext";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../slices/cart.slice";
 
 const Search = () => {
+  const dispatch=useDispatch()
   const { query } = useContext(GlobalContext);
+  const navigate=useNavigate()
 
   const [results, setResults] = useState([]);
 
+
+  const handleAddToCart=(value)=>{
+    dispatch(addToCart(value))
+  }
+
   useEffect(() => {
     fetchSearch();
-  }, []);
+  }, [query]);
 
   const fetchSearch = async () => {
     const { data } = await axios.get(`product/searchproducts?search=${query}`);
     setResults(data.products);
   };
+  const viewProduct=(id)=>{
+    navigate("/product",{state:{id}})
+  }
 
   return (
     <div className="container mt-4">
@@ -25,7 +41,7 @@ const Search = () => {
         </h5>
       </div>
       <div className="searchSection">
-        <div>
+        {/* <div>
           {results.length > 0
             ? results.map((value) => {
                 return (
@@ -50,7 +66,44 @@ const Search = () => {
                 );
               })
             : "No Match Found"}
-        </div>
+        </div> */}
+
+        <Row>
+          {results.length > 0
+            ? results.map((value) => {
+                return (
+                  <Col
+                    className="produc"
+                    key={value._id}
+                    xs={9} sm={7} md={4} lg={3} xl={2} xxl={2}
+                  >
+                    <Card height="100%">
+                      <Card.Img
+                        onClick={() => viewProduct(value._id)}
+                        variant="top"
+                        height="150px"
+                        src={`http://localhost:8000/public/products/${value.image}`}
+                      />
+                      <Card.Body>
+                        <Card.Title onClick={() => viewProduct(value._id)}>
+                          {value.name}
+                        </Card.Title>
+                        <div className="cardSection">
+                          <span>Buy</span>
+                          <span
+                            className="mx-2"
+                            onClick={() => handleAddToCart(value)}
+                          >
+                            Add
+                          </span>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })
+            : "No Product Found"}
+        </Row>
       </div>
     </div>
   );
